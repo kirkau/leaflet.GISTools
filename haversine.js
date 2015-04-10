069f309bc4ba1d7b1756a25b23576cd8aa94893f
kirkau <kirk@gsdata.com.au>
2015-04-10 18:13:43 +1000

    Update haversine.js

--- haversine.js
+++ haversine.js
@@ -2,12 +2,12 @@ function getHaversineDistance(point1, point2){
 	var R = 6372.8;
 
 	var dLat = (point2.lat - point1.lat).toRadians();
-  var dLon = (point2.lon - point1.lon).toRadians();
-  var lat1 = point1.lat.toRadians();
-  var lat2 = point2.lat.toRadians();
+  	var dLon = (point2.lon - point1.lon).toRadians();
+  	var lat1 = point1.lat.toRadians();
+  	var lat2 = point2.lat.toRadians();
 
 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2)
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
 	
 	return = R * c; 	
 }
